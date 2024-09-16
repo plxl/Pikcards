1041d08594b2882832532047e58a5f2346978a6a
Apricone <cyrion@home.nl>
2024-09-16 17:24:04 +0200

    ADD base attributes to card subclasses

---
+++ cards/bases/exploration.py
@@ -0,0 +1,64 @@
+from card import *
+
+# Base exploration data, stats and actions
+class Exploration(Card):
+    def __init__(self,
+                 set,
+                 number,
+                 fifth,
+                 rarity,
+                 name,
+                 image,
+                 cardclass,
+                 base_energy,
+                 base_time,
+                 elements,
+                 immunities,
+                 traits,
+
+                 description: str
+                 ):
+        # Attributes for Card class
+        self.set = set
+        self.number = number
+        self.fifth = fifth
+        self.rarity = rarity
+        self.name = name
+        self.image = image
+        self.cardclass = cardclass
+
+        self.base_energy = base_energy  # Standard energy cost of the card
+        self.energy = base_energy  # current energy cost of the card
+        self.base_time = base_time  # Standard time cost of the card
+        self.time = base_time  # Current time cost of the card
+
+        self.elements = elements
+        self.immunities = immunities
+        self.traits = traits
+
+        self.description = description
+        self.baseDesc = description
+
+        self.owner: int = -1  # Owner player, p1 is 0, p2 is 1
+        self.lane_index: int = -1  # Lane this is currently in. -1 for cards outside the field.
+        
+        # Attributes unique to Areas
+        
+        # Modifiers
+
+    
+
+    # Provides description that shows what the abilities of the class are
+    @abstractmethod
+    def getDescription(self):
+        fullDescription: str = "Exploration\n"
+        fullDescription += self.description
+        return fullDescription
+    
+    # Reset stats for when a card is Returned or Discarded
+    @abstractmethod
+    def resetStats(self):
+        self.lane_index = -1
+        self.energy = self.base_energy
+        self.time = self.base_time
+        self.description = self.baseDesc
